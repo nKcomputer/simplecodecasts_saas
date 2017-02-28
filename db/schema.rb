b3dbd57321c2b25a09d300344dcd12c01a829dff
Ed Stack <stack.edward@gmail.com>
2017-02-28 00:57:56 +0000

    Recreate db migration create Contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223235037) do
+ActiveRecord::Schema.define(version: 20170228005326) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
